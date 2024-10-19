f6c2b4b414231e27e94913608e977f3b42fac762
Vyacheslav Kostrov <vyacheslav.kostrov@x5.ru>
2024-10-20 01:33:35 +0300

    update

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -7,14 +7,14 @@ echo "Create hadoop user..."
 # создаем пользователя hadoop
 sudo adduser --gecos "" hadoop
 # переключаемся на пользователя hadoop
-sudo -i -u hadoop bash << EOF
-
+sudo -i -u hadoop bash -c '
 echo "Generate ssh key for hadoop user"
-# генерируем ssh ключ для пользователя hadoop
+# Generate ssh key for user hadoop
 yes | ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_rsa
 
 echo "Copy public ssh key for next steps"
-# выводи public ssh ключ в stdout
-cat /home/hadoop/.ssh/id_rsa.pub
+# Output public ssh key to stdout
+cat ~/.ssh/id_rsa.pub
+'
 
 exit 0
