a70440622383e427e8b2f1da6841ce548a8ded69
Vyacheslav Kostrov <vyacheslav.kostrov@x5.ru>
2024-10-20 01:24:17 +0300

    [ADD] common script

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,13 +4,17 @@ set -e
 
 echo "Create hadoop user..."
 
+# создаем пользователя hadoop
 sudo adduser --gecos "" hadoop
+# переключаемся на пользователя hadoop
 sudo -i -u hadoop bash << EOF
 
 echo "Generate ssh key for hadoop user"
+# генерируем ssh ключ для пользователя hadoop
 yes | ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_rsa
 
 echo "Copy public ssh key for next steps"
-cat /home/hadoop/.ssh/id_rsa
+# выводи public ssh ключ в stdout
+cat /home/hadoop/.ssh/id_rsa.pub
 
 exit 0
--- scripts/update-hosts.sh
+++ scripts/update-hosts.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# проверяем, что передан файл с хостами
 if [ -z "$1" ]; then
   echo "Usage: $0 <file_with_hosts>"
   exit 1
 fi
 
+# проверяем, что файл существует
 if [ ! -f "$1" ]; then
   echo "Error: File '$1' not found!"
   exit 1
 fi
 
+# сохраняем бэкап исходного файла
 sudo cp /etc/hosts /etc/hosts.bak
+# заменяем hosts
 sudo cp "$1" /etc/hosts
 
+# завершаем
 if [ $? -eq 0 ]; then
   echo "/etc/hosts replaced successfully."
   exit 0
