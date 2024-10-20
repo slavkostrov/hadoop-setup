e885cf83d7ef702a48094613bac342638debd580
Vyacheslav Kostrov <vyacheslav.kostrov@x5.ru>
2024-10-20 11:44:34 +0300

    [ADD] download at same script

--- scripts/setup-nodes.sh
+++ scripts/setup-nodes.sh
@@ -8,10 +8,19 @@ fi
 HOSTNAMES_FILE="$1"
 MAIN_USER="team"
 
+DIST_NAME="hadoop-3.4.0.tar.gz"
+DIST_LINK="https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/$DIST_NAME"
+
 # настраиваем текущую ноду
 echo "Setup current node"
 bash update-hosts.sh "$HOSTNAMES_FILE"
 bash create-user.sh | tail -n1 > "/home/hadoop/.ssh/authorized_keys"
+
+# скачиваем дистрибутив hadoop
+echo "Download hadoop dist..."
+sudo wget -P /home/hadoop "$DIST_LINK" -q --show-progress
+echo "Extract hadoop dist..."
+sudo tar -xzf /home/hadoop/$DIST_NAME -C /home/hadoop/ --checkpoint=.100 --checkpoint-action=dot
 
 current_hostname=$(hostname)
 current_ip=$(hostname -I | awk '{print $1}')
@@ -35,10 +44,16 @@ for host in "${hosts[@]}"; do
 
     # копируем все нужные файлы
     sshpass -p "$ssh_password" scp create-user.sh "$HOSTNAMES_FILE" $MAIN_USER@$host:/
-    sshpass -p "$ssh_password" ssh $MAIN_USER@$host 'bash /update-hosts.sh $HOSTNAMES_FILE'
+    sshpass -p "$ssh_password" ssh $MAIN_USER@$host "bash /update-hosts.sh $HOSTNAMES_FILE"
 
     # нв выходе записываем public ssh ключ с удаленного хоста на локальный хост
     sshpass -p "$ssh_password" ssh $MAIN_USER@$host 'bash /create-user.sh' | tail -n1 >> "/home/hadoop/.ssh/authorized_keys"
+
+    # копируем дистрибутив скаченный
+    sshpass -p "$ssh_password" scp "$DIST_NAME" $MAIN_USER@$host:/home/hadoop
+
+    # распаковываем
+    sshpass -p "$ssh_password" ssh $MAIN_USER@$host "sudo tar -xzf /home/hadoop/$DIST_NAME -C /home/hadoop/ --checkpoint=.100 --checkpoint-action=dot"
 
 done
 
